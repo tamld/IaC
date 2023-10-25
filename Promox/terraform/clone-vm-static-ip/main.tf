b74e5c120b3de8a6b0ae765c376b521affef6682
tamld <ductam1828@gmail.com>
2023-10-25 12:38:50 +0700

    rearrange folder structure

---
+++ Promox/terraform/clone-vm-static-ip/main.tf
@@ -0,0 +1,71 @@
+# Config terraform privder plugin
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "2.9.14"
+    }
+  }
+}
+
+# Proxmox config
+provider "proxmox" {
+  pm_api_url = var.pm_api_url
+  pm_api_token_id = var.token_id
+  pm_api_token_secret = var.token_secret
+  pm_tls_insecure = true # Disable TLS verification while connecting to the proxmox server.
+  # pm_otp - (Optional; or use environment variable PM_OTP) The 2FA OTP code.
+  # pm_otp = true 
+  pm_log_enable = true
+  pm_log_file = "crash.log"
+  pm_debug = true
+  pm_log_levels = {
+    _default = "debug"
+    _capturelog = ""
+ }
+}
+#resource is formatted to be "[type]" "[entity_name]" so in this case
+resource "proxmox_vm_qemu" "test-server-1"  {
+  count       = 1
+  name        = "${var.vm_name}-${var.vm_os}-${count.index + var.vm_vmid}"
+  target_node = var.proxmox_host
+  clone = var.template_name
+  onboot = false # Whether to have the VM startup after the PVE node starts.
+  oncreate = true #Whether to have the VM startup after the VM is created.
+  agent = 1 
+  os_type = "cloud-init" #os_type options: ubuntu, centos or cloud-init
+  cores = var.vm_cores
+  sockets = var.vm_sockets
+  memory = var.vm_memory
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  } 
+  ipconfig0 = "ip=192.168.100.79/24,gw=192.168.100.99"
+  
+  #virtio-scsi-pci (high perfomance mode), virtio-scsi-single, lsi (compatible mode)
+  scsihw = "virtio-scsi-pci" # for high perfomance disk
+  
+  disk {
+    type = "scsi"
+    storage = "zfs"
+    size = "80G"
+    backup = true
+  }
+
+  lifecycle {
+    ignore_changes = [disk, network]
+  }
+  
+  ## Username, password, ssh
+  ciuser     = var.username
+  cipassword = var.password
+
+  sshkeys = join("\n", var.ssh_keys)
+
+  timeouts {
+      create = "5m"
+      update = "7m"
+      delete = "3m"
+  }
+}
---
+++ Promox/terraform/clone-vm-static-ip/var.tf
@@ -0,0 +1,82 @@
+# the variable for terraform
+variable "vm_vmid" {
+  type        = string
+  default     = 100
+  description = "Starting value for vmid"
+}
+
+variable "vm_number" {
+  type    = number
+  default = 1
+  description = "The number of VMs will be created, 0 mean destroy"
+}
+variable "vm_cores" {
+  type    = number
+  default = 1
+  description = "The number of VMs CPU core"
+}
+
+variable "vm_sockets" {
+  type    = number
+  default = 1
+  description = "The number of VMs CPU sockets"
+}
+
+variable "vm_memory" {
+  type    = number
+  default = 2048
+  description = "The amount of VMs RAM"
+}
+
+variable "ssh_keys" {
+  description = "List of public SSH keys"
+  type        = list(string)
+  default     = []
+}
+
+variable "proxmox_host" {
+  description = "Proxmox host IP address or hostname."
+  type        = string
+}
+
+variable "template_name" {
+  description = "Name of the virtual machine template."
+  type        = string
+}
+
+variable "token_id" {
+  description = "Proxmox API token ID."
+  type        = string
+}
+
+variable "token_secret" {
+  description = "Proxmox API token secret."
+  type        = string
+}
+
+variable "pm_api_url" {
+  description = "URL of the Proxmox API."
+  type        = string
+}
+
+variable "vm_name" {
+  description = "Prefix for the virtual machine os."
+  type        = string
+  default     = "VM"
+}
+
+variable "vm_os" {
+  description = "Prefix for the virtual machine os."
+  type        = string
+  default     = "Ubuntu"
+}
+
+variable "username" {
+  description = "Username for the virtual machine."
+  type        = string
+}
+
+variable "password" {
+  description = "Password for the virtual machine"
+  type        = string
+}
