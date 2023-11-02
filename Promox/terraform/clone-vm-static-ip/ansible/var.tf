e7849a2119358a4b6300a9f0e1c39146287d0b64
tamld <ductam1828@gmail.com>
2023-11-02 13:05:52 +0700

    update code for clone-vm-static-ip

--- Promox/terraform/clone-vm-static-ip/ansible/var.tf
+++ Promox/terraform/clone-vm-static-ip/ansible/var.tf
@@ -1,39 +1,9 @@
 # the variable for terraform
-variable "vm_name" {
-  description = "Prefix for the virtual machine os."
-  type        = string
-  default     = "VM"
-}
-
-variable "vm_os" {
-  description = "Prefix for the virtual machine os."
-  type        = string
-  default     = "Ubuntu"
-}
-
-variable "username" {
-  description = "Username for the virtual machine."
-  type        = string
-}
-
-variable "password" {
-  description = "Password for the virtual machine"
-  type        = string
-}
-# variable "user" {
-#   description = "The dynamic string search from ansible inventory.ini file"
-# }
-
-# variable "host_ip" {
-#   description = The dynamic string search from ansible inventory.ini file"
-# }
-
 variable "vm_vmid" {
   type        = string
   default     = 100
   description = "Starting value for vmid"
 }
-
 variable "vm_number" {
   type    = number
   default = 1
@@ -41,19 +11,19 @@ variable "vm_number" {
 }
 variable "vm_cores" {
   type    = number
-  default = 2
+  default = 1
   description = "The number of VMs CPU core"
 }
 
 variable "vm_sockets" {
   type    = number
-  default = 2
+  default = 1
   description = "The number of VMs CPU sockets"
 }
 
 variable "vm_memory" {
   type    = number
-  default = 4096
+  default = 2048
   description = "The amount of VMs RAM"
 }
 
@@ -62,39 +32,41 @@ variable "ssh_keys" {
   type        = list(string)
   default     = []
 }
-
 variable "proxmox_host" {
   description = "Proxmox host IP address or hostname."
   type        = string
 }
-
 variable "template_name" {
   description = "Name of the virtual machine template."
   type        = string
 }
-
 variable "token_id" {
   description = "Proxmox API token ID."
   type        = string
 }
-
 variable "token_secret" {
   description = "Proxmox API token secret."
   type        = string
 }
-
 variable "pm_api_url" {
   description = "URL of the Proxmox API."
   type        = string
 }
-
-variable "ip_address" {
-  description = "IP address of the VM"
+variable "vm_name" {
+  description = "Prefix for the virtual machine os."
+  type        = string
+  default     = "VM"
 }
-variable "gateway" {
-  description = "Gateway of the VM"
+variable "vm_os" {
+  description = "Prefix for the virtual machine os."
+  type        = string
+  default     = "Ubuntu"
 }
-
-variable "subnet_mask" {
-  description = "Subnetmask of the VM"
+variable "username" {
+  description = "Username for the virtual machine."
+  type        = string
+}
+variable "password" {
+  description = "Password for the virtual machine"
+  type        = string
 }
