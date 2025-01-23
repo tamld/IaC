131d61d08b12e49ae56d9e9dc267033955ba4bf3
tamld <ductam1828@gmail.com>
2025-01-24 01:47:43 +0700

    Add PCT scripts

--- Promox/bash/destroy-vm/destroyCT.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Destroy all CT with VMID smaller than 200
-for i in $(pct list | awk '{ print $1 }' | tail -n +2); do
-    if [ $i -lt 200 ]; then
-        echo "Destroying CT with ID: $i"
-        pct stop $i
-        pct destroy $i
-    fi
-done
